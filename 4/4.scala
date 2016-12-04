b774614c79a6d27ae731fd07b2f7dae42763eaef
Kyle Dooley <kylehdooley@gmail.com>
2016-12-04 03:52:54 -0500

    Completed Day 4

--- 4/4.scala
+++ 4/4.scala
@@ -11,20 +11,28 @@ object Day2 {
     }
 
     def main(args: Array[String]) : Unit = {
-        val lines = openFile()
-        var sumIds = 0
-        for (line <- lines) {
-            val temp = line.split('[')
-            val temp2 = temp(0).split('-')
+        val rooms = openFile()
+                    .map(line => {
+                        val temp = line.split('[')
+                        val checksum = temp(1).replace("]", "")
+                        val temp2 = temp(0).split('-')
+                        val id = temp2.last.toInt
+                        (temp2.dropRight(1).mkString("-"), id, checksum)})
+        val validRooms = filterRooms(rooms)
+        println(validRooms.foldLeft(0) {(acc, room) => acc + room._2}) //Part 1
 
-            val id = temp2.last.toInt
+        validRooms.find(room => decrypt(room._1, room._2) == "northpole object storage") match {
+            case Some(room) => println(room._2)
+            case None => println("No location found")
+        }
+    }
+
+    def filterRooms(rooms: Array[(String, Int, String)]) : Array[(String, Int, String)] = {
+        rooms.filter(room => {
             var charCount = Map[Char, Int]()
-            val checksum = temp(1).replace("]", "")
-            for (substring <- temp2.dropRight(1)) {
-                for (c <- substring) {
-                    charCount = charCount + (c -> (charCount.getOrElse(c, 0) + 1))
-                }
-            }
+            room._1.foreach(c => {
+                if (c != '-') charCount = charCount + (c -> (charCount.getOrElse(c, 0) + 1))
+            })
             val checksumCand = charCount
                                .values
                                .toList
@@ -36,8 +44,15 @@ object Day2 {
                                .map(characters => characters.sorted.mkString(""))
                                .mkString("")
                                .take(5)
-            if (checksum == checksumCand) sumIds += id
-        }
-        println(sumIds)
+            room._3 == checksumCand})
+    }
+
+    def decrypt(encrypted: String, offset: Int) : String = {
+        val alphabet = 'a' to 'z'
+        var decrypted = ""
+        encrypted.foreach(c =>
+            if (c == '-') decrypted += ' '
+            else decrypted += alphabet((alphabet.indexOf(c) + offset) % 26))
+        decrypted
     }
 }
